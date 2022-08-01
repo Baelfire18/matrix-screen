6a220139577f00e771cb97b1cc7dc486f4c8a749
Jose Antonio Castro <jacastro18@uc.cl>
2022-08-01 19:37:34 -0400

    refactor: descriptable variables

--- script.js
+++ script.js
@@ -2,29 +2,29 @@
 const canvas = document.getElementById('matrix');
 const ctx = canvas.getContext('2d');
 
-// set the width and height of the canvas
-const w = canvas.width = document.body.offsetWidth;
-const h = canvas.height = document.body.offsetHeight;
+// Set the width and height of the canvas
+const width = canvas.width = document.body.offsetWidth;
+const height = canvas.height = document.body.offsetHeight;
 
-// draw a black rectangle of width and height same as that of the canvas
-ctx.fillStyle = '#000';
-ctx.fillRect(0, 0, w, h);
+// Draw a black rectangle of width and height same as that of the canvas
+ctx.fillStyle = '#000f';
+ctx.fillRect(0, 0, width, height);
 
-const cols = Math.floor(w / 20) + 1;
-const ypos = Array(cols).fill(0);
+const columns = Math.floor(width / 20) + 1;
+const yPos = Array(columns).fill(0);
 
 function matrix() {
   // Draw a semitransparent black rectangle on top of previous drawing
   ctx.fillStyle = '#0001';
-  ctx.fillRect(0, 0, w, h);
+  ctx.fillRect(0, 0, width, height);
 
   // Set color to green and font to 15pt monospace in the drawing context
   ctx.fillStyle = '#0f0';
   ctx.font = '15pt monospace';
 
-  // for each column put a random character at the end
-  ypos.forEach((y, ind) => {
-    // generate a random character
+  // For each column put a random character at the end
+  yPos.forEach((y, ind) => {
+    // Generate a random character
     const text = String.fromCharCode(Math.random() * 128);
 
     // x coordinate of the column, y coordinate is already given
@@ -33,11 +33,11 @@ function matrix() {
     ctx.fillText(text, x, y);
 
     // randomly reset the end of the column if it's at least 100px high
-    if (y > 100 + Math.random() * 10000) ypos[ind] = 0;
+    if (y > 100 + Math.random() * 10000) yPos[ind] = 0;
     // otherwise just move the y coordinate for the column 20px down,
-    else ypos[ind] = y + 20;
+    else yPos[ind] = y + 20;
   });
 }
 
-// render the animation at 40 FPS.
+// Render the animation at 40 FPS.
 setInterval(matrix, 40);
